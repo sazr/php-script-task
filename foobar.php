57ffbd0f2241cfab93aa9cd1fd27e29e70c06846
sazr <samzielkeryner@gmail.com>
2024-03-17 13:24:14 +1000

    Implement task 2

--- foobar.php
+++ foobar.php
@@ -1,3 +1,20 @@
 <?php
 
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 3 == 0 && $i % 5 == 0) {
+        echo "foobar";
+    } elseif ($i % 3 == 0) {
+        echo "foo";
+    }
+    elseif ($i % 5 == 0) {
+        echo "bar";
+    } else {
+        echo $i;
+    }
+
+    if ( $i % 100 != 0) {
+        echo ", ";
+    }
+}
+
 ?>

aaa485384dd6ffddaefa0b54f86f899cf9d0dd91
sazr <samzielkeryner@gmail.com>
2024-03-17 16:01:12 +1000

    Add more robust email validation

--- user_upload.php
+++ user_upload.php
@@ -212,7 +212,10 @@ USAGE;
                 return false;
             }
             // Validate email.
-            // if ( empty( preg_match('/^\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b$/', $user['email']) ) ) {
+            if ( ! $this->validate_email( $user['email'] ) ) {
+                echo "Invalid email: " . json_encode( $user ) . "\n";
+                return false;
+            }
             
             // Format data.
             $user['email']   = $this->format_email( $user['email'] );
@@ -220,8 +223,6 @@ USAGE;
             $user['surname'] = $this->format_string( $user['surname'], true );
             $users[]         = $user;
         }
-
-        echo "Users" . json_encode($users) . "\n";
 
         fclose($file_handle);
 
@@ -249,6 +250,18 @@ USAGE;
         $pattern  = '/^\s+|[\x00-\x1F\x7F]|[\r\n\t"]|[^\p{L}\p{N}\p{P}\p{S}\p{Z}]|\s+$/u';
         $value    = strtolower( preg_replace( $pattern, '', $value ) );
         return $value;
+    }
+
+    /**
+     * Validate email address. Credit: https://phppot.com/php/php-validate-email/
+     * 
+     * @param string $email
+     * @return bool
+     */
+    public function validate_email($email) {
+        $emailRegex = '/^(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){255,})(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){65,}@)(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E\\pL\\pN]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F\\pL\\pN]|(?:\\x5C[\\x00-\\x7F]))*\\x22))(?:\\.(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E\\pL\\pN]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F\\pL\\pN]|(?:\\x5C[\\x00-\\x7F]))*\\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-+[a-z0-9]+)*\\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-+[a-z0-9]+)*)|(?:\\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\\]))$/iDu';
+
+        return preg_match( $emailRegex, strtolower( $email ) ) === 1;
     }
 }
 

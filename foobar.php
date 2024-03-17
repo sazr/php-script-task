1d86f0b03e74251b07f309b1ccdf12f3a9df7015
sazr <samzielkeryner@gmail.com>
2024-03-17 13:29:30 +1000

    Reactor

--- foobar.php
+++ foobar.php
@@ -1,20 +1,38 @@
 <?php
 
-for ($i = 1; $i <= 100; $i++) {
+// for ($i = 1; $i <= 100; $i++) {
+//     if ($i % 3 == 0 && $i % 5 == 0) {
+//         echo "foobar";
+//     } elseif ($i % 3 == 0) {
+//         echo "foo";
+//     }
+//     elseif ($i % 5 == 0) {
+//         echo "bar";
+//     } else {
+//         echo $i;
+//     }
+
+//     if ( $i % 100 != 0) {
+//         echo ", ";
+//     }
+// }
+
+// Use array_map to achieve the same results above.
+$numbers = range(1, 100);
+$output_arr = array_map(function($i) {
     if ($i % 3 == 0 && $i % 5 == 0) {
-        echo "foobar";
-    } elseif ($i % 3 == 0) {
-        echo "foo";
+        return "foobar";
+    }
+    elseif ($i % 3 == 0) {
+        return "foo";
     }
     elseif ($i % 5 == 0) {
-        echo "bar";
-    } else {
-        echo $i;
+        return "bar";
     }
-
-    if ( $i % 100 != 0) {
-        echo ", ";
+    else {
+        return $i;
     }
-}
+}, $numbers);
+echo implode(", ", $output_arr);
 
 ?>

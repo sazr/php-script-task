d3a752b56e84bfd5d88b34c6ec0cc2dfca060194
sazr <samzielkeryner@gmail.com>
2024-03-18 11:47:33 +1000

    Apply PHPCS and document

--- foobar.php
+++ foobar.php
@@ -1,38 +1,47 @@
 <?php
+/**
+ * Task 2
+ * 
+ * Approach 1: Traditional loop with conditional checks.
+ * Approach 2: Using array_map function to achieve the same results.
+ * 
+ * To run this script in the command line, navigate to the directory where
+ * this script is located and execute the following command:
+ * 
+ *     php foobar.php
+ */
 
-// for ($i = 1; $i <= 100; $i++) {
-//     if ($i % 3 == 0 && $i % 5 == 0) {
+// Approach 1: Traditional loop with conditional checks.
+// for ( $i = 1; $i <= 100; $i++ ) {
+//     if ( $i % 3 == 0 && $i % 5 == 0 ) {
 //         echo "foobar";
-//     } elseif ($i % 3 == 0) {
+//     } elseif ( $i % 3 == 0 ) {
 //         echo "foo";
 //     }
-//     elseif ($i % 5 == 0) {
+//     elseif ( $i % 5 == 0 ) {
 //         echo "bar";
 //     } else {
 //         echo $i;
 //     }
 
-//     if ( $i % 100 != 0) {
+//     if ( $i % 100 != 0 ) {
 //         echo ", ";
 //     }
 // }
 
-// Use array_map to achieve the same results above.
-$numbers = range(1, 100);
-$output_arr = array_map(function($i) {
-    if ($i % 3 == 0 && $i % 5 == 0) {
+// Approach 2: Using array_map to achieve the same results above.
+$numbers = range( 1, 100 );
+$output_arr = array_map( function ( $i ) {
+    if ( $i % 3 == 0 && $i % 5 == 0 ) {
         return "foobar";
-    }
-    elseif ($i % 3 == 0) {
+    } elseif ( $i % 3 == 0 ) {
         return "foo";
-    }
-    elseif ($i % 5 == 0) {
+    } elseif ( $i % 5 == 0 ) {
         return "bar";
-    }
-    else {
+    } else {
         return $i;
     }
-}, $numbers);
-echo implode(", ", $output_arr);
+}, $numbers );
+echo implode( ", ", $output_arr );
 
 ?>
